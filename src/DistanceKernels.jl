22adc75fe289f753a99ee79c662a2ea4ba884dae
Tom <tmbdev@gmail.com>
2022-08-24 15:57:04 -0700

    initial

---
+++ src/DistanceKernels.jl
@@ -0,0 +1,138 @@
+module DistanceKernels
+
+using Pkg; Pkg.activate(".")
+
+using Profile, PProf
+using Statistics
+using ChainRules
+using Zygote
+using CUDA
+using CUDAKernels
+using KernelAbstractions
+using KernelGradients
+using Test
+using Tullio
+
+using Flux, MLDatasets
+using Flux: onehotbatch
+
+newaxis = [CartesianIndex()]
+
+CUDA.allowscalar(false)
+
+const AAAF = AbstractArray{<:AbstractFloat}
+
+fp32(x::CuArray) = convert(CuArray{Float32}, x)
+fp32(x::Array) = convert(Array{Float32}, x)
+
+#######################################################################
+# Euclidean Distance using Linear Algebra Primitives
+#######################################################################
+
+function euclidean_distance(x::AAAF, y::AAAF)
+    x² = sum(x.^2, dims=1)
+    y² = sum(y.^2, dims=1)
+    xy = x' * y
+    return sqrt.(x²' .+ y² .- 2xy)
+end
+
+function euclidean_distance2_zygote(w::AAAF, x::AAAF)
+    @tullio result[i, j] := (w[k, i] - x[k, j])^2
+    return result
+end
+
+#######################################################################
+# Euclidean Distance using Kernels
+#######################################################################
+
+function euclidean_distance2(w::AAAF, x::AAAF)
+    @tullio grad=false result[i, j] := (w[k, i] - x[k, j])^2
+    return result
+end
+
+function euclidean_distance2_pb(w, x)
+    result = euclidean_distance2(w, x)
+    return result, (Δresult) -> begin
+        @assert size(result) == size(Δresult) "$size(result) != $size(Δresult)"
+        @tullio grad=false Δw[k, i] := Δresult[i, j] * 2 * (w[k, i] - x[k, j])
+        @tullio grad=false Δx[k, j] := - Δresult[i, j] * 2 * (w[k, i] - x[k, j])
+        return (ChainRules.NoTangent(), Δw, Δx)
+    end
+end
+
+ChainRules.rrule(::typeof(euclidean_distance2), w, x) = euclidean_distance2_pb(w, x)
+
+#######################################################################
+# p-Distance
+#######################################################################
+
+function p_distance_zygote(w::AAAF, x::AAAF; p::Int64=2)
+    @tullio result[i, j] := abs(w[k, i] - x[k, j])^p
+    return result
+end
+
+function p_distance(w::AAAF, x::AAAF; p::Int64=2)
+    @tullio grad=false result[i, j] := abs(w[k, i] - x[k, j])^p
+    return result
+end
+
+function p_distance_pb(w::AAAF, x::AAAF; p::Int64=2)
+    result = p_distance(w, x; p=p)
+    return result, (Δresult) -> begin
+        @assert size(result) == size(Δresult) "$size(result) != $size(Δresult)"
+        @tullio grad=false Δw[k, i] := + Δresult[i, j] * p * abs(w[k, i] - x[k, j])^(p-1) * sign(w[k, i] - x[k, j])
+        @tullio grad=false Δx[k, j] := - Δresult[i, j] * p * abs(w[k, i] - x[k, j])^(p-1) * sign(w[k, i] - x[k, j])
+        return (ChainRules.NoTangent(), Δw, Δx)
+    end
+end
+
+ChainRules.rrule(::typeof(p_distance), w, x; p=2) = p_distance_pb(w, x; p=p)
+
+#######################################################################
+# Weighted p-Distance
+#######################################################################
+
+function weighted_distance_zygote(w::AAAF, s::AAAF, x::AAAF; p::Int64=2)
+    @tullio result[i, j] := abs(s[k, i] * (w[k, i] - x[k, j]))^p
+    return result
+end
+
+function weighted_distance(w::AAAF, s::AAAF, x::AAAF; p::Int64=2)
+    @tullio grad=false result[i, j] := abs(s[k, i] * (w[k, i] - x[k, j]))^p
+    return result
+end
+
+function weighted_distance_pb(w::AAAF, s::AAAF, x::AAAF; p::Int64=2)
+    result = weighted_distance(w, s, x; p=p)
+    return result, (Δresult) -> begin
+        @assert size(result) == size(Δresult) "$size(result) != $size(Δresult)"
+        @tullio grad=false Δw[k, i] := + Δresult[i, j] * p * abs(s[k, i] * (w[k, i] - x[k, j]))^(p-1) * sign(s[k, i] * (w[k, i] - x[k, j])) * s[k, i]
+        @tullio grad=false Δs[k, i] := + Δresult[i, j] * p * abs(s[k, i] * (w[k, i] - x[k, j]))^(p-1) * sign(s[k, i] * (w[k, i] - x[k, j])) * (w[k, i] - x[k, j])
+        @tullio grad=false Δx[k, j] := - Δresult[i, j] * p * abs(s[k, i] * (w[k, i] - x[k, j]))^(p-1) * sign(s[k, i] * (w[k, i] - x[k, j])) * s[k, i]
+        return (ChainRules.NoTangent(), Δw, Δs, Δx)
+    end
+end
+
+ChainRules.rrule(::typeof(weighted_distance), w, s, x; p=2) = weighted_distance_pb(w, s, x; p=p)
+
+#######################################################################
+# mp distance
+#######################################################################
+
+function mp_distance(w::AAAF, x::AAAF)
+    @tullio (max) result[i, j] := abs(w[k, i] - x[k, j])
+    return result
+end
+
+#######################################################################
+# weighted mp distance
+#######################################################################
+
+heaviside(x::AbstractFloat) = ifelse(x < 0, zero(x), ifelse(x > 0, one(x), oftype(x,0.5)))
+
+function wmp_distance(w::AAAF, s::AAAF, x::AAAF)
+    @tullio (max) result[i, j] := max(abs(w[k, i] - x[k, j]) - s[k, i], 0)
+    return result
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,76 @@
+##
+
+using Pkg; Pkg.activate(".")
+
+using BenchmarkTools
+using Profile, PProf
+using Statistics
+using CUDA
+using CUDAKernels
+using Test
+using Zygote
+
+print(CUDA.functional())
+print(Threads.nthreads())
+
+
+include("DistanceKernels.jl"); DK = DistanceKernels
+
+mode = identity;
+
+xs = randn(Float32, 700, 900) |> mode;
+ws = randn(Float32, 700, 900) |> mode;
+ys = randn(Float32, 700, 1100) |> mode;
+zs = randn(Float32, 900, 1100) |> mode;
+
+println("euclidean_distance")
+
+@btime CUDA.@sync DK.euclidean_distance(xs, ys);
+@btime CUDA.@sync Zygote.pullback(DK.euclidean_distance, xs, ys)[2](zs);
+
+println("euclidean_distance2")
+
+@btime CUDA.@sync DK.euclidean_distance2(xs, ys);
+@btime CUDA.@sync Zygote.pullback(DK.euclidean_distance2_zygote, xs, ys)[2](zs);
+@btime CUDA.@sync Zygote.pullback(DK.euclidean_distance2, xs, ys)[2](zs);
+
+xz, yz = Zygote.pullback(DK.euclidean_distance2_zygote, xs, ys)[2](zs);
+xd, yd = Zygote.pullback(DK.euclidean_distance2, xs, ys)[2](zs);
+@test xz ≈ xd
+@test yz ≈ yd
+
+println("p_distance")
+
+@btime CUDA.@sync DK.p_distance(xs, ys);
+@btime CUDA.@sync Zygote.pullback(DK.p_distance_zygote, xs, ys)[2](zs);
+@btime CUDA.@sync Zygote.pullback(DK.p_distance, xs, ys)[2](zs);
+
+xz, yz = Zygote.pullback(DK.p_distance_zygote, xs, ys)[2](zs);
+xd, yd = Zygote.pullback(DK.p_distance, xs, ys)[2](zs);
+@test xz ≈ xd
+@test yz ≈ yd
+
+println("weighted_distance")
+
+@btime CUDA.@sync DK.weighted_distance(xs, ws, ys);
+@btime CUDA.@sync Zygote.pullback(DK.weighted_distance_zygote, xs, ws, ys)[2](zs);
+@btime CUDA.@sync Zygote.pullback(DK.weighted_distance, xs, ws, ys)[2](zs);
+
+xz, wz, yz = Zygote.pullback(DK.weighted_distance_zygote, xs, ws, ys)[2](zs);
+xd, wd, yd = Zygote.pullback(DK.weighted_distance, xs, ws, ys)[2](zs);
+@test xz ≈ xd
+@test wz ≈ wd
+@test yz ≈ yd
+
+println("mp distance")
+
+@btime CUDA.@sync DK.mp_distance(xs, ys);
+@btime CUDA.@sync Zygote.pullback(DK.mp_distance, xs, ys)[2](zs);
+
+println("weighted mp distance")
+
+@btime CUDA.@sync DK.wmp_distance(xs, ws, ys);
+@btime CUDA.@sync Zygote.pullback(DK.wmp_distance, xs, ws, ys)[2](zs);
+
+##
+
